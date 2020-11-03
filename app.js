1a14edd1a0ed64c3a118101e229cff40731844e3
RafaelMoralesV <rmorales@utem.cl>
2020-11-03 15:58:00 -0300

    Mas pruebas iniciales

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const aux = require("./aux.js");
+let persona = new aux("Rafael", 21);
+
+var http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(persona.toString());
+});
+
+server.listen(3000, () => {
+    console.log("Server inicializado en localhost:3000");
+});
